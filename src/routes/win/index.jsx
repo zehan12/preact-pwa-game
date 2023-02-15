0808b03be607a5ed73184a8537965e37f36f7895
Zehan Khan <zehan9211@gmail.com>
2023-02-15 11:26:24 +0530

    win  component done

--- src/routes/win/index.jsx
+++ src/routes/win/index.jsx
@@ -1,8 +1,21 @@
-const Win = () => (
-    <div>
-        <h1>Win</h1>
-        <p>This is the Win route.</p>
-    </div>
-);
+import { route } from "preact-router";
+import style from "./style.css";
+const Win = () => {
 
+    const startGame = () => {
+        route("/game");
+    };
+
+    return (
+        <div class={style.win}>
+            <div class={style.head}>
+                <div class={style.emoji}>🎉</div>
+                <div>You won!</div>
+            </div>
+            <button class={style.button} onClick={startGame}>
+                New Game
+            </button>
+        </div>
+    );
+}
 export default Win;
