5dbb032ef293b6164279986075cd99d50ac10bac
Zehan Khan <zehan9211@gmail.com>
2023-02-15 11:24:18 +0530

    home component done

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,8 +1,19 @@
-const Home = () => (
-    <div>
-        <h1>Home</h1>
-        <p>This is the Home route.</p>
+import { route } from "preact-router";
+import style from "./style.css";
+
+const Home = () => {
+    const startGame = () => {
+        route("/game");
+    };
+    return (<div class={style.home}>
+        <div class={style.head}>
+            <h2>Match Game</h2>
+        </div>
+        <button class={style.button} onClick={startGame}>
+            New Game
+        </button>
     </div>
-);
+    )
+};
 
 export default Home;
